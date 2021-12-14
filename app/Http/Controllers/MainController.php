f3dda1c7ffe5a9deea3bdfc7003534e55d3d896d
gardhaniissaac <gardhaniissaac@gmail.com>
2021-12-14 19:34:08 +0700

    Initial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index(){
+        return view('welcome');
+    }
+
+    public function get_result_1(){
+
+    }
+
+    public function get_result_2(){
+
+    }
+}
